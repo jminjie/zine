18dd1e20423743c9579b148c2636764b27fdf47a
Jordan <jminjie@umich.edu>
2019-06-25 00:04:44 -0700

    Add new poem

---
+++ selector.js
@@ -0,0 +1,35 @@
+const NUM_POEMS = 2; // one more than max poem value
+window.onload = setPoem();
+
+function getPoemNumber() {
+    url = window.location.href.split('?')
+    if (url.length > 1) {
+        return parseInt(url.pop());
+    } else {
+        return NUM_POEMS-1;
+    }
+}
+
+function previous() {
+    current = getPoemNumber();
+    // trick for making negative modulo cycle
+    previous = ((current - 1) % NUM_POEMS + NUM_POEMS) % NUM_POEMS;
+    window.location.replace('?' + previous);
+}
+
+function next() {
+    current = getPoemNumber();
+    next = (current + 1) % NUM_POEMS;
+    window.location.replace('?' + next);
+}
+
+function setPoem() {
+    poemNumber = getPoemNumber();
+    document.getElementById('testpoem').src = 'poems/' + poemNumber + '.jpg';
+    document.getElementById('testpoem').alt = getTranscription(poemNumber);
+}
+
+function getTranscription(last) {
+    // TODO
+    return "image of poem";
+}
